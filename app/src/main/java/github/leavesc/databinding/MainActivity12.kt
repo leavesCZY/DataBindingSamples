2a8329d782b89f28e95b50d56e7d76e5e23403bf
ZmjDns <292570927@qq.com>
2020-07-28 18:12:36 +0800

    修改了第12课的Recycler的adapter

--- app/src/main/java/github/leavesc/databinding/MainActivity12.kt
+++ app/src/main/java/github/leavesc/databinding/MainActivity12.kt
@@ -2,9 +2,13 @@ package github.leavesc.databinding
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import github.leavesc.databinding.adapter.L12RecyclerAdapter
+import github.leavesc.databinding.databinding.ActivityMain12Binding
 import java.util.*
+import kotlin.random.Random
 
 /**
  * 作者：leavesC
@@ -16,7 +20,7 @@ class MainActivity12 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main12)
+        /*setContentView(R.layout.activity_main12)
         val rvList = findViewById<RecyclerView>(R.id.rvList)
         val userList = mutableListOf<UserBean>()
         for (i in 0..19) {
@@ -25,7 +29,17 @@ class MainActivity12 : AppCompatActivity() {
         }
         val userAdapter = UserAdapter(userList)
         rvList.layoutManager = LinearLayoutManager(this)
-        rvList.adapter = userAdapter
+        rvList.adapter = userAdapter*/
+
+        val binding: ActivityMain12Binding = DataBindingUtil.setContentView(this,R.layout.activity_main12)
+
+        val mData = ArrayList<UserBean>().apply {
+            for(i in 0 until 20){
+                add(UserBean("ZMJname$i","ZMJPass${Random.nextInt(1000)}"))
+            }
+        }
+
+        binding.rvList.adapter = L12RecyclerAdapter(mData)
     }
 
 }
